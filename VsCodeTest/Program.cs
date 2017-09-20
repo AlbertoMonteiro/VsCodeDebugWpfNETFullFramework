6e0a7b64b2bb6347e40b5cb73e42d7b35fd5bfa0
Alberto Monteiro <alberto.monteiro@live.com>
2017-09-19 21:07:02 -0300

    Add files via upload

---
+++ VsCodeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsCodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OI");
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Console.WriteLine("Ola");
+            //InitializeComponent();
+        }
+
+
+        public void ChamarOnClick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Debbug WPF pelo VS Code");
+            Console.WriteLine("\\0//");
+        }
+    }
+}
